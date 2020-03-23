408f0b076f257783c909004fdcf0823342bf7df1
lookrecius <bragin.boris@ya.ru>
2020-03-24 02:44:49 +0300

    Change src/components/postStatus/postStatus.js

--- src/components/postStatus/postStatus.js
+++ src/components/postStatus/postStatus.js
@@ -1,8 +1,10 @@
 import React from "react";
+import {Button} from  "reactstrap";
 import "./postStatus.css"
 const PostStatus = () => {
     return (
         <div className="btn-group">
+            <Button color ='warning'>Все</Button>
             <button type ='button' className='btn btn-warning'>Все</button>
             <button type ='button' className='btn btn-outline-danger'>Понравилось</button>
         </div>
