6eed60f339fdd0e81a948a0f592339fe5e75530e
lookrecius <bragin.boris@ya.ru>
2020-03-24 01:59:57 +0300

    Change style of btn src/components/appHeader/appHeader.js

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./appHeader.css";
 const AppHeader = () => {
     return (
-        <div className="appHeader d-flex">
-            <h1>Borque</h1>
+        <div className="app-header d-flex">
+            <h1>Лукреций Пынь</h1>
             <h2>5 записей из них понравилось</h2>
         </div>
     )
