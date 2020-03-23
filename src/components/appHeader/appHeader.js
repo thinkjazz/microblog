38de2d3fc7a6cdef58a5bb84d3a2e706ad0d6240
lookrecius <bragin.boris@ya.ru>
2020-03-24 02:44:14 +0300

    Change src/components/appHeader/appHeader.js

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./appHeader.css";
+import styled from "styled-components";
+let Header = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  h1 {
+  font-size: 26px;
+  color: ${props=> props.colored ? '#eb3b5a' : '#26de81'}
+  :hover {
+        color:#fdcb6e;
+     }
+  }
+  h2 {
+    font-size: 1.2rem;
+     color: grey;
+  }
+`
+
 const AppHeader = () => {
     return (
-        <div className="app-header d-flex">
+        <Header as ='a'>
             <h1>Лукреций Пынь</h1>
             <h2>5 записей из них понравилось</h2>
-        </div>
+        </Header>
     )
 }
 export default AppHeader;
