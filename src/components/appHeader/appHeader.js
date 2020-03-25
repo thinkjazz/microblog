8da68d5c7cd7d325a7157e6b36d20e61b3269249
lookrecius <bragin.boris@ya.ru>
2020-03-26 01:44:10 +0300

    Added new functions src/components/appHeader/appHeader.js

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -1,29 +1,14 @@
 import React from "react";
 import "./appHeader.css";
-import styled from "styled-components";
-let Header = styled.div`
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
-  h1 {
-  font-size: 26px;
-  color: ${props=> props.colored ? '#eb3b5a' : '#26de81'}
-  :hover {
-        color:#fdcb6e;
-     }
-  }
-  h2 {
-    font-size: 1.2rem;
-     color: grey;
-  }
-`
 
-const AppHeader = () => {
+
+
+const AppHeader = ({liked, allPosts}) => {
     return (
-        <Header as ='a'>
+        <div className="app-header d-flex">
             <h1>Лукреций Пынь</h1>
-            <h2>5 записей из них понравилось</h2>
-        </Header>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
+        </div>
     )
 }
 export default AppHeader;
