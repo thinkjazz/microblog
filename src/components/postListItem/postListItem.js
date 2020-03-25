d8f7e5829662aa8de18fff2fe9616b4bfbca4b49
lookrecius <bragin.boris@ya.ru>
2020-03-26 01:44:55 +0300

    Added new functions src/components/postListItem/postListItem.js

--- src/components/postListItem/postListItem.js
+++ src/components/postListItem/postListItem.js
@@ -2,29 +2,10 @@ import React, {Component} from "react";
 import './postListItem.css'
 
 export default class PostListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        };
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this)
-    }
-    onImportant() {
-        this.setState(({important})=> ({
-            important: !important
-        }))
-    }
-    onLike() {
-        this.setState(({like})=> ({
-            like: !like
-        }))
-    }
+
     render() {
-        let {label, onDelete} = this.props;
-        let {important, like} = this.state;
-        let  classNames = 'app-list-item d-flex justify-content-between';
+        let {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
+        let classNames = 'app-list-item d-flex justify-content-between';
         if (important) {
             classNames += ' important';
         }
@@ -34,12 +15,12 @@ export default class PostListItem extends Component {
         return (
             <div className={classNames}>
             <span className="app-list-item-label"
-            onClick={this.onLike}>
+            onClick={onToggleLiked}>
                 {label}
             </span>
                 <div className="d-flex justify-content-center align-items-center">
                     <button type ="button" className="btn-star btn-sm"
-                    onClick={this.onImportant}>
+                    onClick={onToggleImportant}>
                         <i className="fa fa-star"></i>
                     </button>
                     <button type ="button" className="btn-trash btn-sm"
