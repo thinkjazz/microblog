c9bfdcb3b37a007b0da1c68872a8d9f60f2c5750
lookrecius <bragin.boris@ya.ru>
2020-03-26 03:35:13 +0300

    Added upyachka fuck

--- src/components/postAddForm/postAddForm.js
+++ src/components/postAddForm/postAddForm.js
@@ -15,20 +15,27 @@ export default class PostAddForm extends Component {
         this.setState( {
             text: event.target.value
         } )
+
     }
 
     onSubmit(event) {
         event.preventDefault();
-        this.props.onAdd( this.state.text );
-        this.setState({
-            text: ''
-        })
+        if (this.state.text !== '') {
+            this.props.onAdd( this.state.text );
+
+        } else {
+            this.setState({
+                text: 'УПЯЧКА!!! Я ИДИОТ, УБЕЙТЕ МЕНЯ КТО-НИБУДЬ!!!1'
+            })
+        }
+
     }
 
     render() {
         return (
             <form className="bottom-panel d-flex"
                   onSubmit={this.onSubmit}>
+
                 <input
                     type="text"
                     placeholder="О чём вы думаете"
